efed8cac069a7e5dde44974abd855ad63357a18a
Michael Appleby <michael.appleby@me.com>
2022-04-29 20:01:00 -0400

    Improve manifest card with icons, fixed height.

--- src/components/ManifestCard.tsx
+++ src/components/ManifestCard.tsx
@@ -1,22 +1,47 @@
-import {Card, CardContent, CardMedia, Typography} from "@mui/material";
+import {Box, Card, CardContent, CardMedia, Typography} from "@mui/material";
 import React from "react";
 import {Manifest} from "../iiif/Manifest";
+import MapIcon from '@mui/icons-material/Map';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 type ManifestCardProps = {
     manifest: Manifest
 }
 
-const ManifestCard = ({manifest}: ManifestCardProps) => <Card variant="outlined">
+const ManifestCard = ({manifest}: ManifestCardProps) =>
+    <Card variant="outlined"
+          raised={true}
+    >
         <CardMedia
             component="img"
-            image={manifest?.thumb(800)}
+            image={manifest?.thumb(200)}
             alt={manifest.label()}
             height={200}
             key={manifest.uri}
         />
         <CardContent>
-            <Typography variant="body1" component="h2" display="block" gutterBottom>{manifest.label()}</Typography>
-            <Typography variant="caption" display="block" gutterBottom>{manifest.navDateYear() || "no date"}</Typography>
+            <Typography
+                variant="caption"
+                component="h2"
+                display="-webkit-box"
+                overflow="hidden"
+                height={60}
+                sx={{"-webkit-line-clamp":3}}
+                mb={2}
+                >{manifest.label()}</Typography>
+            { (manifest.navDateYear()) ?
+                <div>
+                 <CalendarMonthIcon sx={{float:"left"}} />
+                 <Typography variant="caption" display="inline" gutterBottom>{manifest.navDateYear()}</Typography>
+                </div>
+                :
+                <div>
+                    <CalendarMonthIcon color="disabled" sx={{float:"left", opacity: '50%'}} />
+                </div>
+            }
+            <Box sx={{clear:"left"}}>
+                { (manifest.navPlace()) ? <MapIcon/> : <MapIcon color="disabled" sx={{opacity: '50%'}} /> }
+            </Box>
         </CardContent>
     </Card>;
 
