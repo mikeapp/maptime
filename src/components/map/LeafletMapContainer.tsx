1d3932d161d6fdaf6c163c1dfc4248b4517082bf
Michael Appleby <michael.appleby@me.com>
2022-05-03 09:22:34 -0400

    Move Leaflet CSS from app to component.

--- src/components/map/LeafletMapContainer.tsx
+++ src/components/map/LeafletMapContainer.tsx
@@ -3,6 +3,7 @@ import Map from "./Map";
 import React from "react";
 import {Manifest} from "../../iiif/Manifest";
 import {Box, Stack, Typography} from "@mui/material";
+import "./LeafletMapContainer.css";
 
 interface MapContainerProps {
     manifests:Manifest[];
