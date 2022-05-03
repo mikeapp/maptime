13dc91b917f91ecc56ad99d675d62172ba738381
Michael Appleby <michael.appleby@me.com>
2022-05-03 09:20:00 -0400

    Convert map copyright to typography and remove custom CSS.

--- src/components/map/MapCopyright.tsx
+++ src/components/map/MapCopyright.tsx
@@ -1,9 +1,9 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import React from "react";
 
 const MapCopyright = () =>
     <Box display="block" p={2} sx={{clear:"left"}}>
-        <p className="leafletMapCopyright">Map data &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors</p>
+        <Typography variant="caption">Map data &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors</Typography>
     </Box>
 
 export default MapCopyright;
